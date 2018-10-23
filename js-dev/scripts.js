bcc87cb747ab8c73171a5388faf17ffc2ee3aeb4
Henry puttick <hputtick@me.com>
2018-10-24 00:21:08 +0700

    scripts v3 for animated chevron

--- js-dev/scripts.js
+++ js-dev/scripts.js
@@ -1,23 +1,56 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
 	// Check for click events on the navbar burger icon
-	$(".navbar-burger").click(function() {
-  
+	$(".navbar-burger").click(function () {
+
 		// Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
 		$(".navbar-burger").toggleClass("is-active");
 		$(".navbar-menu").toggleClass("is-active");
-  
+
 	});
 
-	$("#service").click(function() {
+	$("#service").click(function () {
 		$('html, body').animate({
 			scrollTop: $("#services").offset().top
 		}, 500);
 	});
-	
-	
+
+
 });
 
-$(window).scroll(function(){
+$(window).scroll(function () {
 	$("#main").css("opacity", 1 - $(window).scrollTop() / 400);
-  });
+});
+
+$(window).scroll(function () {
+	var numPix = 400; // number of pixels before bottom of page that you want to start fading
+	var op = (($(document).height() - $(window).height()) - $(window).scrollTop()) / numPix;
+	if (op <= 0) {
+		$(".arrow").hide();
+	} else {
+		$(".arrow").show();
+	}
+	$(".arrow").css("opacity", op);
+});
+
+if ($('#back-to-top').length) {
+	var scrollTrigger = 400, // px
+		backToTop = function () {
+			var scrollTop = $(window).scrollTop();
+			if (scrollTop > scrollTrigger) {
+				$('#back-to-top').addClass('show');
+			} else {
+				$('#back-to-top').removeClass('show');
+			}
+		};
+	backToTop();
+	$(window).on('scroll', function () {
+		backToTop();
+	});
+	$('#back-to-top').on('click', function (e) {
+		e.preventDefault();
+		$('html,body').animate({
+			scrollTop: 0
+		}, 700);
+	});
+}
